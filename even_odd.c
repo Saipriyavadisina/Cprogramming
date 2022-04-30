9f1f7ae630c024bf07baa0748692f469a782266b
Sai_Priya <75333911+Saipriyavadisina@users.noreply.github.com>
2022-05-01 00:27:45 +0530

    Check a number is even or odd

---
+++ even_odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(i%2 == 0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+    return 0;
+}
