616b54eb1e494d7fcacf57f271bc2c5215176dcb
Sai_Priya <75333911+Saipriyavadisina@users.noreply.github.com>
2022-05-01 00:27:57 +0530

    checking a number is maximum among given 3 numbers

---
+++ maximum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d", &a,&b,&c);
+    if(a>b && a>c)
+    {
+        if(b>c)
+        {
+            printf("%d is maximum", a);
+        }
+    }
+    else if(b>a && b>c)
+    {
+        if(a>c)
+        {
+            printf("%d is maximum", b);
+        }
+    }
+    else
+    {
+        printf("%d is maximum", c);
+    }
+    return 0;
+}
